9d3efd83a4da0bd3a60a48b9eb477e9e574628a6
ananthanandanan <ananthanandanan@gmail.com>
2023-01-14 23:52:35 +0530

    Blog: MongoDB and expressJS setup

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="text-sm font-medium uppercase hover:text-primary-600 ">
+      <a className="mx-1 text-sm font-medium uppercase hover:text-primary-600">
         {text.split(' ').join('-')}
       </a>
     </Link>

dbb1a7d6a8b7426f550126ee508a6419f3b469a1
ananthanandanan <ananthanandanan@gmail.com>
2024-06-22 19:42:07 +0530

    fix: codemod fix-2

--- components/Link.js
+++ components/Link.js
@@ -6,7 +6,7 @@ const CustomLink = ({ href, ...rest }) => {
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return <Link href={href} {...rest}></Link>
+    return <Link href={href} {...rest} legacyBehavior></Link>
   }
 
   if (isAnchorLink) {
--- components/Tag.js
+++ components/Tag.js
@@ -6,6 +6,7 @@ const Tag = ({ text }) => {
     <Link
       href={`/tags/${kebabCase(text)}`}
       className="mx-1 text-sm font-medium uppercase hover:text-primary-600"
+      legacyBehavior
     >
       {text.split(' ').join('-')}
     </Link>
